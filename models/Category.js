aa5823bc0d01c96a3e9ebea74585b98cd491dc17
Austin Carter <98193003+auscarter17@users.noreply.github.com>
2022-05-04 13:16:51 -0400

    updated product model with ids with datatypes and validations

--- models/Category.js
+++ models/Category.js
@@ -6,7 +6,18 @@ class Category extends Model {}
 
 Category.init(
   {
-    // define columns
+    id: {
+      // id will be auto-incrementing integer that serves as primary key and cannot be null
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+    
   },
   {
     sequelize,
